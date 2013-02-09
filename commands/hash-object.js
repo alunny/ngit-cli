e6213d58668abdb60d2119882e88fdc85b31cdec
alunny <alunny@gmail.com>
2013-02-08 20:59:12 -0800

    respect -w option in hash-object

--- commands/hash-object.js
+++ commands/hash-object.js
@@ -1,4 +1,7 @@
-var createBlob = require('ngit-plumbing/git/createBlob');
+var createBlob = require('ngit-plumbing/git/createBlob'),
+    mkdirp = require('mkdirp'),
+    fs = require('fs'),
+    zlib = require('zlib');
 
 module.exports = function hashObject(args) {
     console.log(args);
@@ -8,7 +11,21 @@ module.exports = function hashObject(args) {
         process.stdin.setEncoding('utf8');
 
         collect(process.stdin, function (err, data) {
-            console.log(createBlob(data));
+            var sha = createBlob(data), dir;
+
+            if (args.indexOf('-w') >= 0) {
+                dir = '.git/objects/' + sha.slice(0,2);
+
+                mkdirp(dir, function (err) {
+                    if (err) throw err;
+
+                    zlib.deflate(data, function (err, compressed) {
+                        if (err) throw err;
+
+                        fs.writeFileSync(dir + '/' + sha.slice(2), compressed);
+                    });
+                });
+            }
         });
     }
 }
