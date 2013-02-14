c30b0c5181e95980e975e9773ca38710ea9a2e59
alunny <alunny@gmail.com>
2013-02-13 21:49:09 -0800

    first pass at cat-file
    * -t works okay
    * -p has to lose the header

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 var args, cmd, commands = {};
 
 commands['hash-object'] = require('./commands/hash-object');
+commands['cat-file']    = require('./commands/cat-file');
 
 // skip "node" and "ngit"
 args = process.argv.slice(2);
--- commands/cat-file.js
+++ commands/cat-file.js
@@ -1,5 +1,18 @@
-var provideContent = require('ngit-plumbing/git/provideContent');
+var provideContent = require('ngit-plumbing/git/provideContent'),
+    objectType = require('ngit-plumbing/git/objectType');
 
 module.exports = function catFile(args) {
+    var sha = args[args.length - 1],
+        objDir = '.git/objects';
+
+    if (args.indexOf('-p') >= 0) {
+        provideContent(sha, objDir, function (err, data) {
+            console.log(data.toString());
+        });
+    } else if (args.indexOf('-t') >= 0) {
+        objectType(sha, objDir, function (err, type) {
+            console.log(type);
+        });
+    }
 
 }
